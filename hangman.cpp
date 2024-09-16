d91b1349f5f770938a1cd9dd66919c78ed10cf10
sahajpokharel <sahaj.pokharel@gmail.com>
2024-09-16 15:35:34 +0545

    Added a function to extract a random word from a file

--- hangman.cpp
+++ hangman.cpp
@@ -1,17 +1,57 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <fstream>
+
 using namespace std;
+
+void display_hangman();
+
+class words{
+	int i;
+	public:
+		void set_i(int value){i=value;}
+		string generate_random_word()
+		{
+			srand(time(0));
+			int word_position = 1 + (rand() % 100);
+			ifstream wordfile;
+			wordfile.open("words.txt");
+			if(!wordfile.is_open())
+			{
+				cout<<"Sorry, unable to generate a word."<<endl;
+				exit(1);
+			}
+			string word;
+		    int current_line = 0;
+		    while (getline(wordfile, word)) 
+			{
+		    	current_line++;
+		        if (current_line == word_position)
+		        break;
+		    }
+		    wordfile.close();
+		    return word;
+		}
+};
 
 int main()
 {
-	cout<<"__________"<<endl;
+	words w;
+	string word;
+	word=w.generate_random_word();
+	return 0;
+}
+
+void display_hangman()
+{
+	cout<<"=========="<<endl;
 	cout<<"    |"<<endl;
 	cout<<"    |"<<endl;
 	cout<<"    O"<<endl;
 	cout<<"   /";
 	cout<<"|";
 	cout<<"\\"<<endl;
-//	cout<<"    |"<<endl;	//For a taller hangman
 	cout<<"   /";
 	cout<<" \\";
-	return 0;
 }

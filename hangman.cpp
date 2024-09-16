a2a7195bf0541784242edceb9a15f09689592ac0
sahajpokharel <sahaj.pokharel@gmail.com>
2024-09-16 14:04:43 +0545

    Initial hangman design

---
+++ hangman.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"__________"<<endl;
+	cout<<"    |"<<endl;
+	cout<<"    |"<<endl;
+	cout<<"    O"<<endl;
+	cout<<"   /";
+	cout<<"|";
+	cout<<"\\"<<endl;
+//	cout<<"    |"<<endl;	//For a taller hangman
+	cout<<"   /";
+	cout<<" \\";
+	return 0;
+}
